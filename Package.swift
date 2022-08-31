b22f319e9d13a5d340e7afff5fdb15ca800197ae
fwcd <fwcdmail@gmail.com>
2022-08-31 15:28:18 +0200

    Implement RoboClientDemo

--- Package.swift
+++ Package.swift
@@ -23,6 +23,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.5.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -36,6 +37,7 @@ let package = Package(
         .executableTarget(
             name: "RoboClientDemo",
             dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .target(name: "RoboClient"),
             ]
         ),
--- Sources/RoboClientDemo/RoboClientDemo.swift
+++ Sources/RoboClientDemo/RoboClientDemo.swift
@@ -1,6 +1,35 @@
+import ArgumentParser
+import Dispatch
+import Foundation
+import RoboClient
+
 @main
-struct RoboClientDemo {
-    static func main() {
-        print("Hello")
+struct RoboClientDemo: ParsableCommand {
+    @Argument(help: "The URL of the Robo server")
+    var url: URL
+
+    func runAsync() async throws {
+        let client = try await RoboClient.connect(to: url)
+        try await client.send(action: .keySequence(text: "Hello world!"))
+    }
+
+    func run() {
+        let task = Task {
+            try await withTaskCancellationHandler {
+                try await runAsync()
+            } onCancel: {
+                print("Cancelled")
+            }
+        }
+
+        // Register interrupt (ctrl-c) handler
+        let source = DispatchSource.makeSignalSource(signal: SIGINT)
+        source.setEventHandler {
+            task.cancel()
+            Self.exit()
+        }
+        source.resume()
+
+        dispatchMain()
     }
 }
