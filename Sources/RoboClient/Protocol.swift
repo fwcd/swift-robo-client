da3074e6d3d780c418a0d1c39ce956197f980968
fwcd <fwcdmail@gmail.com>
2022-08-31 17:49:14 +0200

    Implement basic REPL

--- Sources/RoboClient/Protocol.swift
+++ Sources/RoboClient/Protocol.swift
@@ -1,17 +1,24 @@
 public struct Vec2<Element> {
     public let x: Element
     public let y: Element
+
+    public init(x: Element, y: Element) {
+        self.x = x
+        self.y = y
+    }
 }
 
 extension Vec2: Codable where Element: Codable {}
+extension Vec2: Equatable where Element: Equatable {}
+extension Vec2: Hashable where Element: Hashable {}
 
-public enum MouseButton: String, Codable {
+public enum MouseButton: String, Codable, Hashable {
     case left = "Left"
     case middle = "Middle"
     case right = "Right"
 }
 
-public enum Action: Codable {
+public enum Action: Codable, Hashable {
     case keySequence(text: String)
     case mouseMoveTo(point: Vec2<Int>)
     case mouseMoveBy(delta: Vec2<Int>)
--- Sources/RoboClientDemo/RoboClientDemo.swift
+++ Sources/RoboClientDemo/RoboClientDemo.swift
@@ -9,29 +9,57 @@ private let log = Logger(label: "RoboClientDemo")
 @main
 struct RoboClientDemo: ParsableCommand {
     @Argument(help: "The URL of the Robo server")
-    var url: URL
+    var url: URL = URL(string: "ws://localhost:19877")!
 
     func runAsync() async throws {
         let client = try await RoboClient.connect(to: url)
-        try await client.send(action: .keySequence(text: "Hello world!"))
+
+        let commands: [String: ([String]) async throws -> Void] = [
+            "keys": { args in
+                let text = args.joined(separator: " ")
+                try await client.send(action: .keySequence(text: text))
+            },
+            "moveto": { args in
+                guard args.count == 2,
+                      let x = Int(args[0]),
+                      let y = Int(args[1]) else {
+                    print("Syntax: [x] [y]")
+                    return
+                }
+                try await client.send(action: .mouseMoveTo(point: .init(x: x, y: y)))
+            },
+            "moveby": { args in
+                guard args.count == 2,
+                      let dx = Int(args[0]),
+                      let dy = Int(args[1]) else {
+                    print("Syntax: [dx] [dy]")
+                    return
+                }
+                try await client.send(action: .mouseMoveBy(delta: .init(x: dx, y: dy)))
+            },
+        ]
+
+        log.info("Available commands: \(commands.keys.sorted().joined(separator: ", "))")
+        log.info("Entered REPL, press Ctrl-D to exit.")
+
+        while let line = readLine() {
+            guard !line.isEmpty else { continue }
+            let parsed = line.split(separator: " ").map(String.init)
+            let command = parsed[0]
+            let args = Array(parsed.dropFirst())
+            guard let command = commands[command] else {
+                log.warning("Command '\(command)' not found!")
+                continue
+            }
+            try await command(args)
+        }
     }
 
     func run() {
-        let task = Task {
-            try await withTaskCancellationHandler {
-                try await runAsync()
-            } onCancel: {
-                log.info("Cancelled")
-            }
-        }
-
-        // Register interrupt (ctrl-c) handler
-        let source = DispatchSource.makeSignalSource(signal: SIGINT)
-        source.setEventHandler {
-            task.cancel()
+        Task {
+            try! await runAsync()
             Self.exit()
         }
-        source.resume()
 
         dispatchMain()
     }
