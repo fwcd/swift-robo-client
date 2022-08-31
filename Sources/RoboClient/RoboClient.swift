9eb7a9f72c7ea555e97a7b2e3a46c4df4319f21c
fwcd <fwcdmail@gmail.com>
2022-08-31 14:59:47 +0200

    Implement RoboClient

--- Sources/RoboClient/RoboClient.swift
+++ Sources/RoboClient/RoboClient.swift
@@ -1,6 +1,52 @@
-public struct RoboClient {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import NIO
+import WebSocketKit
 
-    public init() {
+/// A connection to a Robo server.
+public final class RoboClient {
+    private let eventLoopGroup: any EventLoopGroup
+    private let webSocket: WebSocket
+
+    private init(
+        eventLoopGroup: any EventLoopGroup,
+        webSocket: WebSocket
+    ) {
+        self.eventLoopGroup = eventLoopGroup
+        self.webSocket = webSocket
+    }
+
+    deinit {
+        try! webSocket.close().wait()
+        try! eventLoopGroup.syncShutdownGracefully()
+    }
+
+    /// Connects to the Robo server at the given URL.
+    public static func connect(to url: URL) async throws -> Self {
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        let webSocket = try await withCheckedThrowingContinuation { cont in
+            WebSocket.connect(to: url, on: eventLoopGroup) { ws in
+                cont.resume(returning: ws)
+            }.whenFailure { error in
+                cont.resume(throwing: error)
+            }
+        }
+        
+        // TODO: Register listeners
+
+        return Self(eventLoopGroup: eventLoopGroup, webSocket: webSocket)
+    }
+
+    /// Sends an action to the server.
+    public func send(action: Action) async throws {
+        let data = try JSONEncoder().encode(action)
+        guard let json = String(data: data, encoding: .utf8) else {
+            throw RoboClientError.couldNotEncode(data)
+        }
+        try await send(raw: json)
+    }
+
+    /// Sends textual data to the server.
+    private func send(raw: String) async throws {
+        webSocket.send(raw)
     }
 }
---
+++ Sources/RoboClient/RoboClientError.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public enum RoboClientError: Error {
+    case couldNotEncode(Data)
+}
