411c638cc3b0ee6bb94b055bf4b14849962111ec
fwcd <fwcdmail@gmail.com>
2022-09-16 10:17:21 +0300

    Use vendored swift-crypto for iOS backdeploy

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.4"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.4"),
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"3.0.0"),
+        .package(url: "https://github.com/fwcd/swift-crypto.git", branch: "fwcd/ios-backdeploy"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
